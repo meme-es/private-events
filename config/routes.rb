2a222f0b2cd041902710e271933cb95880b719f1
Meme <meme_es@hotmail.com>
2020-05-21 13:07:38 -0600

    Add Session controller and the view for new users
    This commit adds a new Session controller and the view to create a new
    user, also setting-up the right routes to signup, login, and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :events
+  resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'events#index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
