3a58a6efe6943674afac12d4e81ece987f46ff86
bertil291utn <bertilt4@gmail.com>
2020-05-25 11:14:46 -0500

    Separate those events - (“Previously attended events”) from those which are occurring in the future - (“Upcoming events”).

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   has_many :attendances, foreign_key: :event_id
   has_many :attendees, through: :attendances, source: :attendee
+
+  scope :upcoming, -> { where('date > ?', Date.today) }
+  scope :past, -> { where('date < ?', Date.today) }
 end
