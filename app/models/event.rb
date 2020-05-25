e84c08d9022e3cdb8e2be8e3e686e92a7cb9ed66
Meme <meme_es@hotmail.com>
2020-05-25 14:28:30 -0600

    Improve the general events list
    Thi commit creates a new pastevents partial view to list the past events
    and kee the upcoming events in the events partial view, with additional
    validation to disable some buttons.

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ApplicationRecord
   has_many :attendances, foreign_key: :event_id
   has_many :attendees, through: :attendances, source: :attendee
 
-  scope :upcoming, -> { where('date > ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
   scope :past, -> { where('date < ?', Date.today) }
 end
