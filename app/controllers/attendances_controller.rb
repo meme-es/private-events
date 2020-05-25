dc72af66090984e83f4d7490c2d14027df7a7c82
Meme <meme_es@hotmail.com>
2020-05-25 11:12:22 -0600

    Add the mechanism to let the user attend events
    This commit adds a new Attendances controller with a create action to
    set the attending events for a user.

---
+++ app/controllers/attendances_controller.rb
@@ -0,0 +1,9 @@
+class AttendancesController < ApplicationController
+
+  def create
+    @attendance = Attendance.new(event_id: params[:event_id], user_id: params[:user_id])
+    @attendance.save
+
+    redirect_to events_url, notice: 'Event added in your attending events'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :events
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
+  resources :attendances, only: %i[create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
 
