4740f2075dc28256acb493a8c82ebff9b31e5c86
bertil291utn <bertilt4@gmail.com>
2020-05-26 11:54:32 -0500

    Add validation to user model - Presence all user model fields - Uniqueness username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  validates :fullname, presence: true
+  validates :email, :username, uniqueness: true, presence: true
   has_many :events
   has_many :attendances, foreign_key: :user_id
   has_many :attended_events, through: :attendances
