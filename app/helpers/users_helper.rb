a81c49a19f90d83358f990120e465a0b998d95a9
bertil291utn <bertilt4@gmail.com>
2020-05-26 10:19:39 -0500

    Merge fix with feature origin branch

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,5 @@
 module UsersHelper
   def current_user
-<<<<<<< HEAD
-    user = User.find(session[:user_id])
-=======
     User.find(session[:user_id])
->>>>>>> 24762b80f5f942cee8ce934c9b60b5762837023d
   end
 end
