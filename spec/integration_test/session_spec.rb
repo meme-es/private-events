aae0f2305660ef6591acaf8d8a51fbf82708a906
Meme <meme_es@hotmail.com>
2020-05-27 11:24:34 -0600

    Add event and session spec tests
    In this commit we added test specs for session event:
    -  A new user signup
    -  log out
    
    for events:
    - create a new event
    - list user events
    - list user attending events

--- spec/integration_test/session_spec.rb
+++ spec/integration_test/session_spec.rb
@@ -1,14 +1,31 @@
 require_relative '../rails_helper.rb'
 
 RSpec.describe 'Sessions controller', type: :feature do
-  before :each do
-    User.create(fullname: 'Ana A', username: 'ana', email: 'ana@gmail.com')
+  scenario 'create a new user using the signup page' do
+    visit signup_path
+    fill_in 'user[fullname]', with: 'Ana Rivas'
+    fill_in 'user[username]', with: 'ana'
+    fill_in 'user[email]', with: 'ana@email.com'
+    click_button 'Create User'
+    expect(page).to have_content('Login')
   end
 
-  scenario 'signs me in' do
+  scenario 'when a user login' do
+    User.create(fullname: 'Ana A', username: 'ana', email: 'ana@gmail.com')
+
     visit root_path
     fill_in 'Username', with: 'ana'
     click_button 'Login'
     expect(page).to have_content('All Events')
+  end
+
+  scenario 'when a user log out' do
+    User.create(fullname: 'Ana A', username: 'ana', email: 'ana@gmail.com')
+
+    visit root_path
+    fill_in 'Username', with: 'ana'
+    click_button 'Login'
+    click_link 'Log Out'
+    expect(page).to have_content('Login')
   end
 end
